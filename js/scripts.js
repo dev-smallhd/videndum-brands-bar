680738c5b09114cb9fd1dc8ff8a897ec85f2272f
Harvinder Singh <harvinder@smallhd.com>
2017-11-14 18:20:08 +0530

    First commit

---
+++ js/scripts.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){
+
+	$(".vitec-menu h3").click(function()
+	{
+
+		if ($(this).hasClass('active')) {
+				$(".vitec-menu h3").removeClass('active');
+				$(".vitec-menu > li > ul").slideUp();
+		}
+
+		else {
+				$(".vitec-menu h3").removeClass('active');
+				$(".vitec-menu > li > ul").slideUp();
+				$(this).parent().find('ul').slideDown();
+				$(this).addClass('active');
+		}
+	});
+
+	$('.vitec-sites').click(function(){ menuactivity() });
+	$('.close-vitec-menu').click(function(){ menuactivity() });});
+
+	$(window).resize(function(){
+		if($(window).width()<=768)
+		{
+			$('ul.vitec-menu > li > ul').removeAttr('style');
+		}
+	});
+
+	function menuactivity()
+	{
+		$('.vitec-list').slideToggle();
+		$('.vitec-sites').toggleClass('active');
+		$('.vitec-list').toggleClass('open');
+	}
---
+++ readme.txt
@@ -0,0 +1,4 @@
+1. Delete jquery library from index.html if its already on the site.
+2. Delete inline css styles if css reset is in use already. 
+
+Additional js and css files are minified.
