fbb0daa8024dc274b0c364fdf5ce599ba1db095d
Rahul Sharma <rahul.prlxweb@gmail.com>
2023-04-20 17:35:20 +0530

    Replace vitec with videndum

--- js/scripts.js
+++ js/scripts.js
@@ -1,38 +1,38 @@
 $(document).ready(function(){
 
-	$(".vitec-menu h3").click(function()
+	$(".videndum-menu h3").click(function()
 	{
 
 		if($(window).width()<=768)
 		{
 			if ($(this).hasClass('active')) {
-					$(".vitec-menu h3").removeClass('active');
-					$(".vitec-menu > li > ul").slideUp();
+					$(".videndum-menu h3").removeClass('active');
+					$(".videndum-menu > li > ul").slideUp();
 			}
 
 			else {
-					$(".vitec-menu h3").removeClass('active');
-					$(".vitec-menu > li > ul").slideUp();
+					$(".videndum-menu h3").removeClass('active');
+					$(".videndum-menu > li > ul").slideUp();
 					$(this).parent().find('ul').slideDown();
 					$(this).addClass('active');
 			}
 		}
 	});
 
-	$('.vitec-sites').click(function(){ menuactivity() });
-	$('.close-vitec-menu').click(function(){ menuactivity() });
+	$('.videndum-sites').click(function(){ menuactivity() });
+	$('.close-videndum-menu').click(function(){ menuactivity() });
 });
 
 	$(window).resize(function(){
 		if($(window).width()<=768)
 		{
-			$('ul.vitec-menu > li > ul').removeAttr('style');
+			$('ul.videndum-menu > li > ul').removeAttr('style');
 		}
 	});
 
 	function menuactivity()
 	{
-		$('.vitec-list').slideToggle();
-		$('.vitec-sites').toggleClass('active');
-		$('.vitec-list').toggleClass('open');
+		$('.videndum-list').slideToggle();
+		$('.videndum-sites').toggleClass('active');
+		$('.videndum-list').toggleClass('open');
 	}
--- readme.txt
+++ readme.txt
@@ -1,4 +1,4 @@
-1. Change the logo image as needed (images/vitec-logo.png).
+1. Change the logo image as needed (images/videndum-logo.png).
 2. Delete jQuery library from index.html if its already included on the website.
 3. Delete inline css styles if css reset is already used.
 
